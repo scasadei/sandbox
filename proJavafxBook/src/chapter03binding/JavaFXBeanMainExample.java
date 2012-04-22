89d649ec11b7dec9c66a14954f9d9e4bc7db2678
stefano casadei <scasadei@yahoo.com>
2012-04-22 19:08:39 +0200

    created new package and copied several classes into it; added JavaSFX SDK library to classpath via a menu choice in the change build path menu Change-Id: I718a9c04574c1dd719be82fc1644ebd82f383063

---
+++ proJavafxBook/src/chapter03binding/JavaFXBeanMainExample.java
@@ -0,0 +1,15 @@
+package chapter03binding;
+public class JavaFXBeanMainExample {
+    public static void main(String[] args) {
+        JavaFXBeanModelExample model = new JavaFXBeanModelExample();
+        JavaFXBeanViewExample view = new JavaFXBeanViewExample(model);
+        JavaFXBeanControllerExample controller = new JavaFXBeanControllerExample(model, view);
+
+        controller.incrementIPropertyOnModel();
+        controller.changeStrPropertyOnModel();
+        controller.switchColorPropertyOnModel();
+        controller.incrementIPropertyOnModel();
+        controller.changeStrPropertyOnModel();
+        controller.switchColorPropertyOnModel();
+    }
+}
